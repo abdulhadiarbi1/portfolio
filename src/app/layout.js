d2c1bf7f9851e07ed666dff0ec48e3f480142159
abdulhadi240 <ah912425@gmail.com>
2023-08-21 15:45:21 +0500

    favicon changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,9 @@ export default function RootLayout({ children }) {
         {children}
         </Providers>
         <Analytics />
+        <div className='hidden sm:block'>
         <ChatwoptWidget/>
+        </div>
         </body>
       
 
